2d77338813aaaccddaa7fe648cbd6636982ca86d
anjuman raj <ayusmann23@gmail.com>
2022-08-04 09:01:16 +0530

    minor animation change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -72,7 +72,7 @@ export default function Hero() {
                       <AttentionSeeker effect="tada">
                         <NavLink
                           to="/products"
-                          className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                          className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 hover:animate-bounce "
                         >
                           Explore Our Products
                           <BsArrowRightCircle className=" inline text-2xl ml-4 " />
