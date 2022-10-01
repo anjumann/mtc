9938a0c2d0440dff93944eba0f9ac44f1379a765
anjuman raj <ayusmann23@gmail.com>
2022-10-01 14:39:36 +0530

    hero section is updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,99 +4,37 @@ import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { BsArrowRightCircle } from "react-icons/bs";
 import hero from "../../assets/images/hero.jpg";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Slide, Fade, AttentionSeeker } from "react-awesome-reveal";
-
 
 export default function Hero() {
   return (
-    <div className="relative bg-white overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          {/* <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg> */}
-
-          <Popover>
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Popover.Panel
-                focus
-                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-              >
-                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="px-5 pt-4 flex items-center justify-between">
-                    <div>
-                      <img className="h-8 w-auto z-10" src={hero} alt="" />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
-                        <span className="sr-only">Close main menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <main className="mt-10 mx-auto max-w-7xl -z-10 px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
-              <Slide>
-                <Fade>
-                  <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                    <span className="block xl:inline">One Place For </span>{" "}
-                    <span className="block text-red-600 xl:inline">
-                      Your Printing Solutions
-                    </span>
-                  </h1>
-                  <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  We are a printing house that has been in the business of offering a host of offset printing, stamp, paper book binding, Stationary and other similar services. Our company being located in Narhan; we have over 21 years of experience in this sector.
-                  </p>
-                  <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                    <div className="rounded-md shadow">
-                      <AttentionSeeker effect="tada">
-                        <NavLink
-                          to="/products"
-                          className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10 hover:animate-bounce "
-                        >
-                          Explore Our Products
-                          <BsArrowRightCircle className=" inline text-2xl ml-4 " />
-                        </NavLink>
-                      </AttentionSeeker>
-                    </div>
-                  </div>
-                </Fade>
-              </Slide>
+    <>
+      <div className="container px-6 py-16 mx-auto text-center">
+        <div className="max-w-[80vw] mx-auto">
+          <h1 className="text-3xl font-bold text-gray-800  lg:text-4xl">
+            Building Your Next App with our Awesome components
+          </h1>
+          <p className="mt-6 text-gray-500 text-2xl">
+            We are a printing house that has been in the business of offering a
+            host of offset printing, stamp, paper book binding, Stationary and
+            other similar services. Our company being located in Narhan; we have
+            over 21 years of experience in this sector.
+          </p>
+          <Link to="/products" className="" >
+            <div className="px-6 py-2.5 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-red-600 rounded-lg hover:bg-red-500 lg:mx-0 lg:w-auto focus:outline-none  ">
+              Explore Our products
             </div>
-          </main>
+          </Link>
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <img
+            className="object-cover w-full h-96 rounded-xl lg:w-4/5"
+            src={hero}
+          />
         </div>
       </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <Slide direction="right">
-          <Fade>
-            <img
-              className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-              src={hero}
-              alt=""
-            />
-          </Fade>
-        </Slide>
-      </div>
-    </div>
+    </>
   );
 }
